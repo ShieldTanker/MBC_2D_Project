b23021b066172dbdf7f0c376e68f285076bf0191
ShieldTanker <168048193+ShieldTanker@users.noreply.github.com>
2026-08-04 17:55:28 +0900

    움직임 추가 및 레이어 추가

--- Assets/Resourcese/Scripts/Movement2D.cs
+++ Assets/Resourcese/Scripts/Movement2D.cs
@@ -1,18 +1,22 @@
 ﻿using UnityEngine;
-
 public class Movement2D : MonoBehaviour
 {
     private Vector2 _moveInput = Vector2.zero;
     private Rigidbody2D _rb;
-
     [SerializeField] private float currentSpeed;
-    private float speed = 20f;      // 움직이는 속력
-    private float accelation = 20f;  // 가속력
+    public float speed = 20f;      // 움직이는 속력
+    public float accelation = 5f;  // 가속력
+    public float _break = 10f;
+
     private bool _canMove = true;
+    private bool _isMoving = false;
+    [SerializeField] private bool _isGrounded = false;
+    [SerializeField] private LayerMask grdLayer;
 
     public bool CanMove { get { return _canMove; } }
-    public float MoveSpeed { get { return speed; } set { speed = value ; } }
-    public float Acceleration { get { return accelation; } set { accelation = value ; } }
+    public float MoveSpeed { get { return speed; } set { speed = value; } }
+    public float Acceleration { get { return accelation; } set { accelation = value; } }
+    public float Gravity { get; set; } = 9.8f;
 
     private void Awake()
     {
@@ -21,6 +25,7 @@ public class Movement2D : MonoBehaviour
 
     void Update()
     {
+        GroundDetect();
         Move();
         currentSpeed = _rb.linearVelocity.magnitude;
     }
@@ -29,19 +34,23 @@ public class Movement2D : MonoBehaviour
     {
         if (_rb == null) return;
 
-        if (_canMove)
-        {
-            Vector2 dest = _moveInput * speed;
-            // _rb.linearVelocity = Vector2.Lerp(_rb.linearVelocity, dest, accelation * Time.deltaTime);
-            _rb.linearVelocity = Vector2.MoveTowards(_rb.linearVelocity, dest, accelation * Time.deltaTime);
-        }
+        Vector2 dest = _isMoving ? _moveInput * speed : Vector2.zero;
+
+        dest.y -= _isGrounded ? 0.2f : Gravity;
+        _rb.linearVelocity = Vector2.Lerp(_rb.linearVelocity, dest, accelation * Time.deltaTime);
     }
 
     public void MoveInput(Vector2 input)
     {
-        if(input.sqrMagnitude > 0)
-            _moveInput = input;
-        else
-            _moveInput = Vector2.zero;
+        if (_canMove)
+        {
+            _moveInput.x = input.x;
+            _isMoving = _moveInput.x != 0;
+        }
+    }
+
+    void GroundDetect()
+    {
+        _isGrounded = Physics2D.Raycast(transform.position, Vector2.down, 0.2f, grdLayer);
     }
 }
---
+++ Assets/Resourcese/Scripts/StateMachine/AgentStateMachine/States/AgentJumpState.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+public class AgentJumpState : State<AgentStateType,AgentContext>
+{
+    public AgentJumpState
+        (StateMachine<AgentStateType, AgentContext> stateMachine,
+        List<StateTransition<AgentStateType, AgentContext>> transitions,
+        List<StateLogic<AgentContext>> logics)
+    {
+        _machine = stateMachine;
+        _transitions = transitions;
+        _logics = logics;
+    }
+}
